3c38d8226905491df48a96df193e2bce43402ac3
giovanniamorim <giovanni.amorim.ti@gmail.com>
2021-10-01 14:54:03 -0300

    Adding page not found component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,13 @@ import { AppComponent } from './app.component';
 import { HeroListComponent } from './hero-list/hero-list.component';
 import { CrisisCenterComponent } from './crisis-center/crisis-center.component';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundError } from 'rxjs';
+import { PageNotFoundComponent } from './erros/page-not-found/page-not-found.component';
 
 const appRoute: Routes = [
   { path: 'crisis-center', component: CrisisCenterComponent },
   { path: 'heroes', component: HeroListComponent },
+  { path: '**', component: NotFoundError },
 ];
 
 @NgModule({
@@ -18,7 +21,12 @@ const appRoute: Routes = [
     FormsModule,
     RouterModule.forRoot(appRoute, { enableTracing: true }),
   ],
-  declarations: [AppComponent, HeroListComponent, CrisisCenterComponent],
+  declarations: [
+    AppComponent,
+    HeroListComponent,
+    CrisisCenterComponent,
+    PageNotFoundComponent,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
