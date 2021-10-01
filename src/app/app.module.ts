236aa947573a74c4242b52c9e96ac1ddeafb4b7e
giovanniamorim <giovanni.amorim.ti@gmail.com>
2021-10-01 14:29:08 -0300

    Adicionando rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { HeroListComponent } from './hero-list/hero-list.component';
+import { CrisisCenterComponent } from './crisis-center/crisis-center.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoute: Routes = [
+  { path: 'crisis-center', component: CrisisCenterComponent },
+  { path: 'heroes', component: HeroListComponent },
+];
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoute, { enableTracing: true }),
+  ],
+  declarations: [AppComponent, HeroListComponent, CrisisCenterComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
